4011f1f132877ba08328b3800a1f3b15db0f4c01
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-22 09:23:55 -0300

    api confing

---
+++ src/api/apiConfig.js
@@ -0,0 +1,9 @@
+const apiConfig = {
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "c1b661944b322e9bff1c1acc8b8e9033",
+
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
+
+export default apiConfig;
