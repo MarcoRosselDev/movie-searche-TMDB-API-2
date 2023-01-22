8cdd91f7787674c2563cdafcccfdae74bca10f38
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-22 11:45:55 -0300

    Routes App js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.1.4/css/boxicons.min.css";
 import "./App.scss";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routes from "./config/Routes";
 
 function App() {
   return (
-    <div>
-      <h3>testing</h3>
-    </div>
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
   );
 }
 
