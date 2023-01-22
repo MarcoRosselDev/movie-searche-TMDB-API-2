e15429c8a29bced8b8513dea7008247762d3125f
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-22 09:40:22 -0300

    tmdb api export 2

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -16,3 +16,18 @@ export const tvType = {
   top_rated: "top_rated",
   on_the_air: "on_the_air",
 };
+
+const tmdbApi = {
+  getMovieList: (type, params) => {
+    const url = "movie/" + movieType[type];
+    return axiosClient.get(url, params);
+  },
+  getTvList: (type, params) => {
+    const url = "tv/" + tvType[type];
+    return axiosClient.get(url, params);
+  },
+  getVideos: (cate, id) => {
+    const url = category[cate] + "/" + id + "/videos";
+    return axiosClient.get(url, { params: {} });
+  },
+};
