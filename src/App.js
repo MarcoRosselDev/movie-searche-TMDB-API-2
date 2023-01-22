4b16b87f5a616f055c7abc4a654f7fc2af63459e
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-22 12:10:49 -0300

    react-router-dom@5.3.0 successful

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import "swiper/swiper.min.css";
 import "./assets/boxicons-2.1.4/css/boxicons.min.css";
 import "./App.scss";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
-import RoutesConfig from "./config/Routes";
+import Routes from "./config/Routes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          render={(props) => (
-            <>
-              <Header {...props} />
-              <RoutesConfig />
-              <Footer />
-            </>
-          )}
-        />
-      </Routes>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
     </BrowserRouter>
   );
 }
--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Detail from "../pages/Detail";
-import Catalog from "../pages/Catalog";
 
-const RoutesConfig = () => {
+import { Route, Switch } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Catalog from "../pages/Catalog";
+import Detail from "../pages/Detail";
+
+const Routes = () => {
   return (
-    <Routes>
-      <Route path="/:category/search/:keyword" element={<Catalog />} />
-      <Route path="/:category/:id" element={<Detail />} />
-      <Route path="/:category" element={<Catalog />} />
-      <Route path="/" exact element={<Home />} />
-    </Routes>
+    <Switch>
+      <Route path="/:category/search/:keyword" component={Catalog} />
+      <Route path="/:category/:id" component={Detail} />
+      <Route path="/:category" component={Catalog} />
+      <Route path="/" exact component={Home} />
+    </Switch>
   );
 };
 
-export default RoutesConfig;
+export default Routes;
