f3740d5843d1856525fdc4e9a85a12ce0af31696
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-23 09:46:24 -0300

    hero slide strucure

--- src/components/hero-slider/HeroSlide.jsx
+++ src/components/hero-slider/HeroSlide.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./hero-slider.scss";
 
+import tmdbApi, { category, movieType } from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
+
 const HeroSlide = () => {
-  return <div>HeroSlide</div>;
+  const [movieItems, setMovieItems] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const params = { page: 1 };
+      try {
+        const response = await tmdbApi.getMovieList(movieType.popular, {
+          params,
+        });
+        setMovieItems(response.results.slice(0, 4));
+        console.log(response);
+      } catch {
+        console.log("error");
+      }
+    };
+    getMovies();
+  }, []);
+
+  return <div className="hero-slide">HeroSlide</div>;
 };
 
 export default HeroSlide;
